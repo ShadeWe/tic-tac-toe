8551750e189f6e0f2c18016a21ec1219651b8401
ShadeWe <slim.shady.ghetto@yandex.ru>
2019-07-12 16:34:37 +0500

    winning large cells

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var largeCells = document.getElementsByClassName('lg-cell');
-
+var cells;	// contains a 3x3 array containing arrays of smallCells.
 /*
  * Returnes an array containing a collection of cells
  * (DOM elements) for putting X or O in it
@@ -29,12 +29,64 @@ function getCells()
 
 	return cells;
 }
+
+/*
+ * Make a large cell a big 'X' or 'O' sign which means this field is won by this sign.
+ * largeCell_id - the id of the largeCell that's just been won
+ * sign - a string containing an 'X' or 'O' character representing the winner of a largeCell
+ */
+function markFieldAsWon(largeCell_id, sign)
+{
+	largeCells[largeCell_id].innerHTML = "<p>" + sign + "</p>";
+	largeCells[largeCell_id].className = 'lg-cell won-cell';
+}
+
+/*
+ * Takes the number of a large cell which a player has made a move in
+ * and checks if a player has won a large cell
+ * if it's true, mark it with a big 'O' or 'X' sign
+ * largeCell - the cell which is being checked for being won
+ */
+function checkForWin(largeCell)
+{
+	// checking for winning horizontaly
+	for (let i = 0; i < 8; i = i + 3) {
+		if (cells[largeCell][i].textContent != '' && cells[largeCell][i + 1].textContent != '' && cells[largeCell][i + 2].textContent != '') {
+			if (cells[largeCell][i].textContent == cells[largeCell][i + 1].textContent && cells[largeCell][i + 1].textContent == cells[largeCell][i + 2].textContent) {
+				return markFieldAsWon(largeCell, cells[largeCell][i].textContent); // passing largeCell id and the sign which has just won the field
+			}
+		}
+	}
+
+	// checking for winning vertically
+	for (let i = 0; i < 3; i++) {
+		if (cells[largeCell][i].textContent != '' && cells[largeCell][i + 3].textContent != '' && cells[largeCell][i + 6].textContent != '') {
+			if (cells[largeCell][i].textContent == cells[largeCell][i + 3].textContent && cells[largeCell][i + 3].textContent == cells[largeCell][i + 6].textContent) {
+				return markFieldAsWon(largeCell, cells[largeCell][i].textContent); // passing largeCell id and the sign which has just won the field
+			}
+		}
+	}
+	// checking for winning diagonally from the left top corner to the right bottom one
+	if (cells[largeCell][0].textContent != '' && cells[largeCell][4].textContent != '' && cells[largeCell][8].textContent != '') {
+		if (cells[largeCell][0].textContent == cells[largeCell][4].textContent && cells[largeCell][4].textContent == cells[largeCell][8].textContent) {
+			return markFieldAsWon(largeCell, cells[largeCell][0].textContent); // passing largeCell id and the sign which has just won the field
+		}
+	}
+	// checking for winning diagonally from the right top corner to the left bottom one
+	if (cells[largeCell][2].textContent != '' && cells[largeCell][4].textContent != '' && cells[largeCell][6].textContent != '') {
+		if (cells[largeCell][2].textContent == cells[largeCell][4].textContent && cells[largeCell][4].textContent == cells[largeCell][6].textContent) {
+			return markFieldAsWon(largeCell, cells[largeCell][2].textContent); // passing largeCell id and the sign which has just won the field
+		}
+	}
+}
+
 /*
  * Append click events to all the buttons
  * The main part of the program takes place here
  *
  */
-function appendEventListeners(cells) {
+function appendEventListeners(cells)
+{
 
 	var currentPlayer= 'X';
 
@@ -66,7 +118,8 @@ function appendEventListeners(cells) {
 							cells[i][j].innerHTML = '<p>O</p>';
 							currentPlayer = 'X'; // the next move is for 'X'
 						}
-						correctStep = j;	 // set the correct large cell for the next move
+						if (correctStep != -1) checkForWin(correctStep);	// checking if a player has won a large cell
+						correctStep = j;	 				// set the correct large cell for the next move
 					}
 				}
 			});
@@ -74,4 +127,5 @@ function appendEventListeners(cells) {
 	}
 }
 
-appendEventListeners(getCells());
+cells = getCells();
+appendEventListeners(cells);
